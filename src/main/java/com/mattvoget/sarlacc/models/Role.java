e66d3267c8f44739cfc37c36de30b503e6ae827a
Matt <matt.voget@gmail.com>
2017-04-20 07:05:34 -0600

    added demo role

--- src/main/java/com/mattvoget/sarlacc/models/Role.java
+++ src/main/java/com/mattvoget/sarlacc/models/Role.java
@@ -3,7 +3,7 @@ package com.mattvoget.sarlacc.models;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    ADMIN, USER;
+    ADMIN, USER, DEMO;
 
     @Override
     public String getAuthority() {
