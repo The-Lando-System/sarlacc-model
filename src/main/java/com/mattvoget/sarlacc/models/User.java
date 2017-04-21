f156644871976df9f0a4e4e29100c43e8ae91742
Matt <matt.voget@gmail.com>
2017-04-21 06:54:52 -0600

    Cleanup

--- src/main/java/com/mattvoget/sarlacc/models/User.java
+++ src/main/java/com/mattvoget/sarlacc/models/User.java
@@ -11,6 +11,8 @@ import java.util.Set;
 
 public class User implements UserDetails {
 	
+	private static final long serialVersionUID = 1L;
+	
 	private String id;
 	private String username;
 	private String password;
